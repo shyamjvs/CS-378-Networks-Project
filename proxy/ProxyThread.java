c3979b31dd6c3e6dfd924c1afb001a90d614b26f
Shyam JVS <shyam123.jvs95@gmail.com>
2014-11-26 22:10:07 +0530

    Added post method to ProxyThread, not yet fully functional

--- proxy/ProxyThread.java
+++ proxy/ProxyThread.java
@@ -27,12 +27,17 @@ public class ProxyThread extends Thread{
             BufferedReader inFromClient = new BufferedReader(new InputStreamReader(socket.getInputStream()));
 			BufferedWriter outToClient = new BufferedWriter(new OutputStreamWriter(socket.getOutputStream()));
 			
-            int lineNumber = 0;
+            int lineNumber = 0;			// Counter for reading through input request lines from client
+			int URLType = 1;			// 1 for HTTPS; 0 for HTTP;
             String clientHTTPRequest;
-            String urlRequested = "";
+            String urlRequested1 = "";	//  URL in HTTPS form
+            String urlRequested2 = "";	//  URL in HTTP form
+			String requestType = "";	// "GET" or "POST"
+			String urlParameters = "";	// Parameters for POST request
 
             // Phase 1 : Wait for the client to give a GET request
             while ((clientHTTPRequest = inFromClient.readLine()) != null){
+
                 // Get the URL from the first line of request
                 try {
                     StringTokenizer t = new StringTokenizer(clientHTTPRequest);
@@ -43,56 +48,72 @@ public class ProxyThread extends Thread{
                 
                 if (lineNumber == 0){
                     String[] tokens = clientHTTPRequest.split(" ");
-                    System.out.println("URL requested by client: " + tokens[1] + " through: " + tokens[0]);
+					requestType = tokens[0];
+                    System.out.println("URL requested by client: " + tokens[1] + " through: " + requestType);
 
-                    if(tokens[1].startsWith("http://") || tokens[1].startsWith("https://")) 
-						urlRequested = tokens[1];
-                    else
-						urlRequested = "https://"+tokens[1];
-
-                    System.out.println("URL sent to web server : " + urlRequested);
+					if(tokens[1].startsWith("https://")) urlRequested1 = tokens[1];
+					else if(tokens[1].startsWith("http://")) urlRequested2 = tokens[1];
+                    else{
+						urlRequested1 = "https://"+tokens[1];
+						urlRequested2 = "http://"+tokens[1];
+					}
                 }
 				lineNumber++;
             }
             // We now have the URL asked by the client ready with us
 
 			BufferedReader readFromWebServer = null; // Read stream for data from web server
+			int responseCode = 0;
+
             try{
 
-				// Phase 2 : Send a GET request to the web server for the URL through HTTPS first
-                System.out.println("Sending GET request (HTTPS) to the web server for: " + urlRequested);
-                URL url = new URL(urlRequested);
-                HttpURLConnection conn = (HttpURLConnection) url.openConnection();
+				// Phase 2 : Send a GET/POST request to the web server for the URL
+				HttpURLConnection conn = null;
 
-        		conn.setRequestMethod("GET");
-        		conn.setRequestProperty("User-Agent", USER_AGENT);
+				// First try HTTPS, if URL available
+				if(!urlRequested1.equals("")){
+					System.out.println("Attempting for HTTPS URL");
+
+					if(requestType.equals("GET")){
+						conn = sendGet(urlRequested1);
+					}
+					else if(requestType.equals("POST")){
+						conn = sendPost(urlRequested1, urlParameters);
+					}
+					else{
+						System.out.println("Unknown/Unsupported request Type: "+requestType);
+					}
        
-        		int responseCode = conn.getResponseCode();
-        		System.out.println("Response Code for HTTPS: " + responseCode);
+	        		responseCode = conn.getResponseCode();
+	        		System.out.println("Response Code for HTTPS: " + responseCode);
+	         		if(responseCode != 200) System.out.println("Request for HTTPS failed");
+				}
+				// Next try HTTP, if URL available and HTTPS failed/URL unavailable
+				if(!urlRequested2.equals("") && responseCode != 200){
+					System.out.println("Attempting for HTTP URL");
 
-				// If request fails, then send through HTTP
-				if(responseCode != 200){
-	         		System.out.println("Request for HTTPS failed");
-					urlRequested = "http" + urlRequested.substring(5);	
-					System.out.println("Sending GET request (HTTP) to the web server for: " + urlRequested);
-					
-                    url = new URL(urlRequested);
-                	conn = (HttpURLConnection) url.openConnection();
-             	
-					conn.setRequestMethod("GET");
-					conn.setRequestProperty("User-Agent", USER_AGENT);
-          
+					if(requestType.equals("GET")){
+						conn = sendGet(urlRequested2);
+					}
+					else if(requestType.equals("POST")){
+						conn = sendPost(urlRequested2, urlParameters);
+					}
+					else{
+						System.out.println("Unknown/Unsupported request Type: "+requestType);
+					}
+
 					responseCode = conn.getResponseCode();
     	     		System.out.println("Response Code for HTTP: " + responseCode);
+	         		if(responseCode != 200) System.out.println("Request for HTTP failed");
 				}					
-				// GET request sent by the proxy
+				// GET/POST request sent by the proxy
 
-                // Phase 3 : Obtain the response from the web server
+                // Phase 3 : Receive the response from the web server
                 InputStream inFromWebServer = conn.getInputStream();
 				readFromWebServer = new BufferedReader(new InputStreamReader(inFromWebServer), BUFFER_SIZE);
 				// Response received from the server and stored into buffer
 
-				// Phase 4 : Send the output received from the web server to the client
+				// Phase 4 : Output the input received from the web server to the client
 				int value = 0;
                 while ((value = readFromWebServer.read()) != -1){
 					outToClient.write(value);
@@ -105,7 +126,7 @@ public class ProxyThread extends Thread{
                 outToClient.write("",0,0);
             }
 
-            // Close all the I/O streams
+            // Close all the I/O streams and writers
             if (readFromWebServer != null) {
                 readFromWebServer.close();
             }
@@ -123,4 +144,49 @@ public class ProxyThread extends Thread{
             e.printStackTrace();
         }
     }
+
+	private HttpURLConnection sendGet(String urlRequested) throws Exception {
+
+		HttpURLConnection conn = null;
+		try{
+			System.out.println("Sending GET request to the web-server for: " + urlRequested);
+			URL url = new URL(urlRequested);
+			conn = (HttpURLConnection) url.openConnection();
+
+   			conn.setRequestMethod("GET");
+   			conn.setRequestProperty("User-Agent", USER_AGENT);
+		} catch (Exception e){
+                System.err.println("Exception in sendGet(): " + e);
+		}
+		return conn;
+	}
+
+	private HttpURLConnection sendPost(String urlRequested, String urlParameters) throws Exception {
+
+		HttpURLConnection conn = null;
+		try{
+			System.out.println("Sending POST request to the web-server for: " + urlRequested);
+			URL url = new URL(urlRequested);
+			conn = (HttpURLConnection) url.openConnection();
+ 	
+   			conn.setRequestMethod("POST");
+			conn.setRequestProperty("User-Agent", USER_AGENT);
+			conn.setRequestProperty("Accept-Language", "en-US,en;q=0.5"); 
+			conn.setRequestProperty("Content-Language", "en-US");
+			conn.setRequestProperty("Content-Type", "application/x-www-form-urlencoded");
+			conn.setRequestProperty("Content-Length", "" + Integer.toString(urlParameters.getBytes().length));
+			conn.setUseCaches (false);
+			conn.setDoInput(true);
+			conn.setDoOutput(true);
+	
+			DataOutputStream wr = new DataOutputStream(conn.getOutputStream());
+			wr.writeBytes(urlParameters);
+			wr.flush();
+			wr.close();
+		} catch (Exception e){
+				System.err.println("Exception in sendPost(): " + e);
+		}
+		return conn;
+	}
+
 }
