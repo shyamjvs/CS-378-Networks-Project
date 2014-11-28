4de9d47c903ec106171782bf532e6506689c7899
--replace-all <virtuoso02031995@gmail.com>
2014-11-28 06:02:19 +0530

    priority queue

---
+++ proxy/Prioritization.java
@@ -0,0 +1,22 @@
+package proxy;
+import java.util.PriorityQueue;
+import java.util.Queue;
+
+public class Prioritization extends Thread {
+public static int cur_queue_size;
+public static Queue<QElement> queue = new PriorityQueue<QElement>();
+private int max_queue_size;
+	public Prioritization(int queue_size) {
+		max_queue_size=queue_size;
+		cur_queue_size=0;
+	}
+		
+public void run(){
+	while(true){
+		if( cur_queue_size < max_queue_size ) {
+			queue.poll().getproxythread().start();
+			cur_queue_size++;
+		}
+	}
+}
+}
--- proxy/ProxyServer.java
+++ proxy/ProxyServer.java
@@ -10,13 +10,18 @@ import java.util.Date;
 public class ProxyServer {
 public static HashMap<String,Long> authenticated = new HashMap<String,Long>();
 public static HashSet<String> login = new HashSet<String>();
+public static HashMap<String,Integer> priorityMap = new HashMap<String,Integer>();
 
 public static void main(String[] args) throws IOException {
 login.add("paramdeep:singh");
+priorityMap.put("192.168.1.9", 100);
 ServerSocket serverSocket = null;
 boolean listening = true;
 BlockedIp.readFile("src/proxy/blocked_ips");
-int port = 7940; //default
+int port = 7940;
+Prioritization prior = new Prioritization(10);
+prior.start();
+//default
 // try {
 // port = Integer.parseInt(args[0]);
 // } catch (Exception e) {
@@ -26,7 +31,11 @@ try {
 serverSocket = new ServerSocket(port);
 System.out.println("Started on: " + port);
 while (listening) {
-new ProxyThread(serverSocket.accept()).start();
+	ProxyThread pt=new ProxyThread(serverSocket.accept());
+	if(priorityMap.containsKey(pt.clien_addr))
+		prior.queue.add(new QElement(pt, priorityMap.get(pt.clien_addr)));
+	else
+		prior.queue.add(new QElement(pt, 10));
 }
 serverSocket.close();
 }
